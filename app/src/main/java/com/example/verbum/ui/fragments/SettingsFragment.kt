95f6bbf4d7879a8a660c525c60666e580fe11ecf
yourmadnesss@mail.ru <Kukuruza134!>
2022-04-13 16:33:02 +0300

    Изменения в личном профиле

--- app/src/main/java/com/example/verbum/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/verbum/ui/fragments/SettingsFragment.kt
@@ -10,16 +10,29 @@ import com.example.verbum.R
 import com.example.verbum.activities.RegisterActivity
 import com.example.verbum.ui.fragments.BaseFragment
 import com.example.verbum.ui.fragments.ChangeNameFragment
+import com.example.verbum.ui.fragments.ChangeUsernameFragment
 import com.example.verbum.utilits.AUTH
+import com.example.verbum.utilits.USER
 import com.example.verbum.utilits.replaceActivity
 import com.example.verbum.utilits.replaceFragment
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
         setHasOptionsMenu(true)
+        initFields()
 
+    }
+
+    private fun initFields() {
+        settings_bio.text = USER.bio
+        settings_full_name.text = USER.fullname
+        settings_phone_number.text = USER.phone
+        settings_status.text = USER.status
+        settings_username.text = USER.username
+        settings_btn_change_username.setOnClickListener { replaceFragment(ChangeUsernameFragment()) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {

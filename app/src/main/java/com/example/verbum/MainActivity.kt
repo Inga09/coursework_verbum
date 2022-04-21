f620dd35138905df077291d010a1c3fe372b0ea5
yourmadnesss@mail.ru <Kukuruza134!>
2022-04-21 16:40:02 +0300

    в сети

--- app/src/main/java/com/example/verbum/MainActivity.kt
+++ app/src/main/java/com/example/verbum/MainActivity.kt
@@ -68,6 +68,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        AppStates.updateState(AppStates.ONLINE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        AppStates.updateState(AppStates.OFFLINE)
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/verbum/utilits/AppStates.kt
@@ -0,0 +1,17 @@
+package com.example.verbum.utilits
+
+enum class AppStates(val state:String) {
+    ONLINE("в сети"),
+    OFFLINE("был недавно"),
+    TYPING("печатает");
+
+    companion object{
+        fun updateState(appStates: AppStates){
+            /*Функция принимает состояние и записывает в базу данных*/
+            FER_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID).child(CHILD_STATE)
+                .setValue(appStates.state)
+                .addOnSuccessListener { USER.state = appStates.state }
+                .addOnFailureListener { showToast(it.message.toString()) }
+        }
+    }
+}

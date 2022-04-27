253a1cbe50bf9e22d43ea90a576ce598094e1d3e
yourmadnesss@mail.ru <Kukuruza134!>
2022-04-27 22:39:16 +0300

    чат

---
+++ app/src/main/java/com/example/verbum/ui/fragments/SingleChatFragment.kt
@@ -0,0 +1,24 @@
+package com.example.verbum.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.verbum.R
+import com.example.verbum.models.CommonModel
+import com.example.verbum.utilits.APP_ACTIVITY
+
+
+class SingleChatFragment (contact: CommonModel): BaseFragment(R.layout.fragment_single_chat) {
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.GONE
+
+    }
+}
